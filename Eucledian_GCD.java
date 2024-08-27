4e2334a6594f0d7181d2d2faca68f435c5dba9de
kavyjain0309 <kj.kavyansh2002@gmail.com>
2024-08-27 12:28:37 +0530

    eucledian gcd

---
+++ Eucledian_GCD.java
@@ -0,0 +1,14 @@
+public class Eucledian_GCD {
+    public static int Gcd(int a, int b){
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+        int n = Gcd(6,27);
+        System.out.println(n);
+    }
+}
